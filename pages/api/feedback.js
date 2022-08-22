0e52a9ec7a05a609f0166b0720ef6604f580470b
Sergey Kochetkov <sergey.zh.kochetkov@gmail.com>
2022-08-21 23:15:04 -0700

    Use api routes inside getStaticProps

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 
+export function buildFeedbackPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json')
+}
+
+export function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath)
+  const data = JSON.parse(fileData)
+  return data
+}
+
 function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email
@@ -13,9 +23,9 @@ function handler(req, res) {
     }
 
     // store in a database
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json')
-    const fileData = fs.readFileSync(filePath)
-    const data = JSON.parse(fileData)
+    const filePath = buildFeedbackPath()
+    const data = extractFeedback(filePath)
+
     data.push(newFeedback)
     fs.writeFileSync(filePath, JSON.stringify(data))
     res.status(201).json({
@@ -23,8 +33,11 @@ function handler(req, res) {
       feedback: newFeedback,
     })
   } else {
+    const filePath = buildFeedbackPath()
+    const data = extractFeedback(filePath)
+
     res.status(200).json({
-      message: 'This works!',
+      feedback: data,
     })
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([])
   const emailInputRef = useRef()
   const feedbackInputRef = useRef()
 
@@ -26,6 +27,14 @@ function HomePage() {
       .then((data) => console.log(data))
   }
 
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback)
+      })
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
@@ -46,6 +55,14 @@ function HomePage() {
         </div>
         <button>Send Feedback</button>
       </form>
+
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   )
 }
